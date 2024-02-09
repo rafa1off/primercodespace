bed4d080b9b81ece0c9daa50649d61d097f2f777
Rafael Giuriatto <rafagiuriatto@hotmail.com>
2024-02-09 00:10:38 +0000

    changes in codespace

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Rafael Giuriatto",
+  title: "Computer Science Student",
+  email: "rafagiuriatto@hotmail.com",
+  gitHub: "rafa1off",
+  instagram: "grtt0",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "giuriatt0r",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
